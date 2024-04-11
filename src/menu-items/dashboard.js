1f36f76a60748bcf3e57b995dc04e04cc7331063
hassen <hassen@hassen.com>
2024-04-11 21:24:41 +0900

    drawer(좌측메뉴창) width 수정

--- src/menu-items/dashboard.js
+++ src/menu-items/dashboard.js
@@ -15,7 +15,7 @@ const dashboard = {
   children: [
     {
       id: 'dashboard',
-      title: 'Dashboard',
+      title: '장비 현황',
       type: 'item',
       url: '/dashboard/default',
       icon: icons.DashboardOutlined,
